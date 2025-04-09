dc76b4c962712d6aa03dd2b6900e312c192d9c22
hypnoshock <hypnoshock@gmail.com>
2025-04-09 23:27:37 +0100

    service which runs the input.py script

--- install.sh
+++ install.sh
@@ -4,3 +4,7 @@ apt update
 apt install xmp screen
 cp -f .bashrc ~/.bashrc
 cp -f config.txt /boot/firmware
+cp -f gpio_keypress.service /etc/systemd/system/
+systemctl daemon-reload
+systemctl enable gpio_keypress
+systemctl start gpio_keypress
